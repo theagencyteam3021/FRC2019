f7e92c70a308bccc227bc6fe91cf7f5a587f63fa
Raj Pabari <52271154+rpabari2020@users.noreply.github.com>
2020-11-11 13:10:09 -0800

    added potentiometer to robotmap

--- FRC2019/src/main/java/frc/robot/RobotMap.java
+++ FRC2019/src/main/java/frc/robot/RobotMap.java
@@ -24,8 +24,7 @@ public class RobotMap {
   public static final int TURRET_SHOOTER = 6;
   public static final int TURRET_FEEDER = 7;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+  // sensors
+  public static final int POTENTIOMETER = 1;
+
 }
