c04389af5e651501d632e29368810f308087a33b
Raj Pabari <52271154+rpabari2020@users.noreply.github.com>
2020-12-13 17:27:12 -0800

    autonomousAssist structure started

--- FRC2019/src/main/java/frc/robot/AutonomousController.java
+++ FRC2019/src/main/java/frc/robot/AutonomousController.java
@@ -29,6 +29,12 @@ public class AutonomousController extends AgencySystem {
     private double distanceToTarget;
     private double netAngle;
 
+    //distances in inches
+    private final double DISTANCE_HORIZONTAL_THRESHOLD = 6.0; //change
+    private final double MAX_DISTANCE_FROM_TARGET = 500.0;
+    private final double MIN_DISTANCE_FROM_TARGET = 12.0;
+    private double limelightYThreshold;
+
     //constant heights for testing
     //note all measurements in inches
     private final double TARGET_HEIGHT = 83.7;
@@ -43,7 +49,7 @@ public class AutonomousController extends AgencySystem {
     private double camVertical;
     private double camHorizontal;
     private double targetAngle;
-    private double distanceToMove;
+    private double distanceHorizontal;
 
     private Queue<Double> potNeckAngleAverager;
     private int potNeckAngleSize;
@@ -80,11 +86,9 @@ public class AutonomousController extends AgencySystem {
 
         potNeckAngleAverager = new LinkedList<Double>();
         potNeckAngleSize = 0;
-        potNeckAngleSum = 0;
-        avgPotNeckAngle = 0;
-        distanceToTarget = 0;
-        netAngle = 0;
-        targetAngle = 0.0;
+        potNeckAngleSum = avgPotNeckAngle = distanceToTarget = netAngle = 0.0;
+        camVertical = camHorizontal = targetAngle = distanceHorizontal = 0.0;
+        limelightYThreshold = 0.0;
     }
 
     private void displayValues() {
@@ -95,7 +99,7 @@ public class AutonomousController extends AgencySystem {
         shuffleDebug("potentiometerNeckAngle", potentiometerNeckAngle);
         shuffleDebug("avgPotNeckAngle", avgPotNeckAngle);
         shuffleDebug("TargetAngle", targetAngle);
-        shuffleDebug("DistanceToMove", distanceToMove);
+        shuffleDebug("DistanceToMove", distanceHorizontal);
         shuffleDebug("camHorizontal", camHorizontal);
         shuffleDebug("camVertical", camVertical);
         shuffleDebug("LimelightHOR", limelightHOR);
@@ -145,8 +149,28 @@ public class AutonomousController extends AgencySystem {
         //Because we're dealing with arccos here, the targetAngle could be negative.
         //FIX: Move the robot a direction, and see how the angle changes.
         targetAngle = Math.toDegrees(Math.acos(camHorizontal / TARGET_HORIZONTAL)) + limelightX;
-        distanceToMove = distanceToTarget * Math.cos(Math.toRadians(targetAngle));
+        distanceHorizontal = distanceToTarget * Math.cos(Math.toRadians(targetAngle));
         return (targetAngle);
+    }
+
+    //@return double array [distance to go left/right, amount to move neck, distance from target, ready to shoot?]
+
+    private double[] autonomousAssist() {
+        double[] ans = new double[4];
+
+        ans[0] = distanceHorizontal;
+        ans[1] = limelightY; // is this correct?
+        ans[2] = distanceToTarget;
+        ans[3] = 0.0; // 0 if not ready to shoot, 1 if ready, -1 if needs to move forward/backwards
+        if (ans[0] <= DISTANCE_HORIZONTAL_THRESHOLD && ans[1] <= limelightYThreshold) {
+            ans[3] = 1.0;
+        }
+        //handle these cases later
+        if (ans[2] >= MAX_DISTANCE_FROM_TARGET || ans[2] <= MIN_DISTANCE_FROM_TARGET) {
+            ans[3] = -1.0;
+        }
+
+        return ans;
     }
 
     //Updates and displays sensor values during teleop
